0d82309c5e1753cb54caf115537cfb9a319de0eb
Filip Havrlent <filip@havrlent.com>
2017-12-14 21:12:13 +0100

    Some layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
+import { Container, Row, Col } from 'reactstrap';
 import './App.css';
 
 class App extends Component {
   render() {
-    return <div className="App" />;
+    return (
+      <Container>
+        <Row>
+          <Col sm="12" md={{ size: 8, offset: 2 }} />
+        </Row>
+      </Container>
+    );
   }
 }
 
