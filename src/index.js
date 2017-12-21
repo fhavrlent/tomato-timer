4648690104c5d408c04d41eed5587b1aad5c8f87
Filip Havrlent <filip@havrlent.com>
2017-12-21 17:49:58 +0100

    Another githug pages try

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { ConnectedRouter } from 'react-router-redux';
-import store, { history } from './store';
+import store from './store';
 import App from './App.js';
+import { BrowserRouter, Redirect, Switch } from 'react-router-dom';
 import { Route } from 'react-router-dom';
 import Timer from './containers/Timer';
 import Settings from './containers/Settings';
@@ -15,14 +15,17 @@ const target = document.querySelector('#root');
 
 render(
   <Provider store={store}>
-    <ConnectedRouter history={history}>
+    <BrowserRouter>
       <div>
         <App>
-          <Route exact path="/" component={Timer} />
-          <Route exact path="/settings" component={Settings} />
+          <Switch>
+            <Route exact path="/" component={Timer} />
+            <Route exact path="/settings" component={Settings} />
+            <Redirect from="*" to="/" />
+          </Switch>
         </App>
       </div>
-    </ConnectedRouter>
+    </BrowserRouter>
   </Provider>,
   target
 );
--- src/store.js
+++ src/store.js
@@ -1,14 +1,10 @@
 import { createStore, applyMiddleware, compose } from 'redux';
-import { routerMiddleware } from 'react-router-redux';
 import thunk from 'redux-thunk';
-import createHistory from 'history/createBrowserHistory';
 import rootReducer from './reducers';
-
-export const history = createHistory();
 
 const initialState = {};
 const enhancers = [];
-const middleware = [thunk, routerMiddleware(history)];
+const middleware = [thunk];
 
 if (process.env.NODE_ENV === 'development') {
   const devToolsExtension = window.devToolsExtension;
