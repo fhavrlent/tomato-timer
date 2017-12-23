c64de863b941c496f30ba6f200ebe33bbb524edd
Filip Havrlent <filip@havrlent.com>
2017-12-23 13:05:45 +0100

    Removed one left merge conflict

--- src/containers/Settings/index.js
+++ src/containers/Settings/index.js
@@ -3,10 +3,6 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Settings from './Settings';
 import { setTime, reset } from '../../dispatchers/SettingActions';
-<<<<<<< HEAD
-=======
-import { changeTimeLeft } from '../../dispatchers/TimerActions';
->>>>>>> master
 
 class SettingsPage extends Component {
   static propTypes = {
