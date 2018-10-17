326756c9060c6a57359586460d49ea06887d073d
Filip Havrlent <havrlent@havrlentmac.domain.local>
2018-10-17 16:56:19 +0200

    For CN Event

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Redirect, Switch, Route } from 'react-router-dom';
 import { Navigation } from './components/Common/';
 import TimerPage from './containers/Timer';
 import SettingsPage from './containers/Settings';
+import SecretContactForm from './containers/SecretContactForm/index';
 
 const App = () => {
   return (
@@ -16,6 +17,7 @@ const App = () => {
         <Switch>
           <Route exact path="/" component={TimerPage} />
           <Route exact path="/settings" component={SettingsPage} />
+          <Route exact path="/zemiak" component={SecretContactForm} />
           <Redirect from="*" to="/" />
         </Switch>
       </main>
---
+++ src/containers/SecretContactForm/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default function SecretContactForm() {
+  return (
+    <form name="contact" netlify>
+      <p>
+        <label>
+          Name <input type="text" name="name" />
+        </label>
+      </p>
+      <p>
+        <label>
+          Email <input type="email" name="email" />
+        </label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
+    </form>
+  );
+}
